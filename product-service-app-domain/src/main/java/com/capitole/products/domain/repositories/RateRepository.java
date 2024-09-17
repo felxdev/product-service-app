309555b6a5a641d7d17ed0a1d81ae10ba93c4431
felix <felxdev@outlook.com>
2024-09-17 13:22:40 +0200

    [PRODUCTAPP-1.1] Modify format definition api, delete unnecessary spring annotation

--- product-service-app-domain/src/main/java/com/capitole/products/domain/repositories/RateRepository.java
+++ product-service-app-domain/src/main/java/com/capitole/products/domain/repositories/RateRepository.java
@@ -1,10 +1,8 @@
 package com.capitole.products.domain.repositories;
 
 import com.capitole.products.domain.model.Rate;
-import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 
-@Repository
 public interface RateRepository {
 
   Flux<Rate> findByProductIdAndBrandIdAndDate(Integer productId, Integer brandId, String date);
